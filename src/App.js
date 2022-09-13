e718a7638a4907845af16f3bdb5a029b3ade0bfe
Megan Schuetz <mschuetz411@gmail.com>
2022-09-13 13:52:17 -0600

    add error handling for main page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react'
 import './App.css';
 import { allData } from './apiCalls'
 import AllStoriesList from './AllStoriesList'
-import Details from './Details';
+import Details from './Details'
+import Error from './Error'
 import { Route } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
@@ -15,29 +16,31 @@ function App() {
     allData
       .then(data => setAllStories(data.results))
       .catch(error => setError(error))
-
-      // console.log('allstories', allStories)
   })
 
   return (
     <div className="App">
-      <header className="App-header">
-       <h1>New York Times Top Stories</h1>
-      </header>
-      
-      <Route exact path='/'>
-        {allStories && <AllStoriesList allStories={allStories}/>}
-      </Route>
+    {error ? <Error /> : 
+      <div>
+        <header className="App-header">
+          <h1>New York Times Top Stories</h1>
+        </header>
+        
+        <Route exact path='/'>
+          {allStories && <AllStoriesList allStories={allStories}/>}
+        </Route>
 
-      <Route path="/:published_date" render={({ match }) => {
-        return (
-        <div>
-          {allStories && <Details pubDate={match.params.published_date} allStories={allStories}/>}
-        </div>
-        )
-        }}/>
-    </div>
-  );
+        <Route exact path="/:published_date" render={({ match }) => {
+          return (
+          <div>
+            {allStories && <Details pubDate={match.params.published_date} allStories={allStories}/>}
+          </div>
+          )
+          }}/>
+      </div>
+      }
+     </div>
+  )
 }
 
 export default App;
---
+++ src/Error.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Error.css'
+
+function Error() {
+
+  return (
+    <div className='error'>
+      <h1>Oh no, sorry, there has been an error.</h1>
+    </div>
+  )
+}
+
+export default Error
